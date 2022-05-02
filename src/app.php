18e8f35f9c9bb00899185163433d3fcc96ead319
chraebsli <nicholas.joel.krebs@gmail.com>
2022-05-02 13:06:47 +0200

    tasks from php

--- src/app.php
+++ src/app.php
@@ -15,24 +15,6 @@ if (validateCurrentSession()){
 }
 $userID = intval($_SESSION['userID']);
 
-$allLists = getAllLists($userID);
-while($row = $allLists->fetch_assoc()) {
-    $listID = $row['listID'];
-    $listName = $row['listName'];
-    $listUserID = $row['listUserID'];
-
-    //echo "$listName ($listID) <br>";
-    $allTasks = getAllTasks($listID);
-    while($row = $allTasks->fetch_assoc()) {
-        $taskID = $row['taskID'];
-        $taskName = $row['taskName'];
-        $taskStatus = $row['taskStatus'];
-
-        //echo "$taskStatus $taskName ($taskID) <br>";
-    }
-    //echo "<br><br>";
-}
-
 ?>
 
 <!DOCTYPE html>
@@ -43,6 +25,7 @@ while($row = $allLists->fetch_assoc()) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="./css/main.css" />
     <link rel="stylesheet" href="./css/elements.css" />
+    <link rel="stylesheet" href="./css/app.css" />
     <link rel="shortcut icon" href="./assets/favicon.svg" />
     <title>Todo list | chraebsli.dev</title>
 </head>
@@ -50,13 +33,75 @@ while($row = $allLists->fetch_assoc()) {
 <body>
     <header id="header"></header>
 
-    <main id="main" class="main-l main-fh f c a col">
+    <main id="main" class="f col">
+        <div class="app" id="app">
+            <?php
+                
+                $allLists = getAllLists($userID);
+                while($row = $allLists->fetch_assoc()) {
+                    $listID = $row['listID'];
+                    $listName = $row['listName'];
+                    $listUserID = $row['listUserID'];
 
+                    // list
+                    echo "<div class='list' id='list-$listID'>";
+                    // list header
+                    echo "<div class='header f a row'><span class='list-title f20'>$listName</span></div>";
+
+                    // open tasks
+                    $allTasks = getAllTasks($listID, 'open');
+                    echo "<div class='content open f col'><div class='items-cont-open'>";
+                    while($row = $allTasks->fetch_assoc()) {
+                        $taskID = $row['taskID'];
+                        $taskName = $row['taskName'];
+                        echo "<div class='items task f a row'>
+                                <input type='checkbox' id='li-$listID-$taskID' name='li-$listID-$taskID' />
+                                <span for='li-$listID-$taskID'>$taskName</span>
+                            </div>";
+                    }
+                    echo "</div></div>";
+
+                    // add task
+                    echo "<div class='add-task'>
+                            <img src='./assets/i/plus.svg' alt='plus icon' class='i' onclick='addTask(this)' />
+                        </div>";
+
+                    // done tasks cont
+                    echo "<div class='content done f col' data-toggle='closed'>";
+                    // header
+                    echo "<div class='toggle-header f a row' onclick='toggleDoneTasks(this)'>
+                            <span>abgehakte</span>
+                            <img src='./assets/i/expand-more.svg' alt='expand icon' class='i toggle-tasks-icon' />
+                        </div>";
+
+                    // done tasks items
+                    $allTasks = getAllTasks($listID, 'done');
+                    echo "<div class='items-cont-done'>";
+                    while($row = $allTasks->fetch_assoc()) {
+                        $taskID = $row['taskID'];
+                        $taskName = $row['taskName'];
+                        echo "<div class='items task f a row'>
+                                <input checked type='checkbox' id='li-$listID-$taskID' name='li-$listID-$taskID' />
+                                <span for='li-$listID-$taskID'>$taskName</span>
+                            </div>";
+                    }
+                    echo "</div>";
+
+                    // close done tasks cont
+                    echo "</div>";
+
+                    // close list
+                    echo "</div>";
+                }
+            ?>
+        </div>
     </main>
 
     <footer id="footer"></footer>
 </body>
 
+<script src="./js/init.js"></script>
 <script src="./js/elements.js"></script>
+<script src="./js/app.js"></script>
 
 </html>
--- src/php/app.php
+++ src/php/app.php
@@ -9,13 +9,26 @@ function validateString($string) {
     return htmlspecialchars(stripslashes(trim($string)));
 }
 
-function getAllLists($userID) {
+function getUserID(){
+    if(isset($_SESSION['userID'])){
+        return $_SESSION['userID'];
+    }
+    return false;
+}
+
+function getAllLists() {
+    $userID = getUserID();
     $sql = "SELECT listID, listName, listUserID FROM TodoApp.Lists WHERE listUserID = $userID";
     return SQLQuery($sql);
 }
 
-function getAllTasks($listID) {
-    $sql = "SELECT taskID, taskName, taskStatus FROM TodoApp.Tasks WHERE taskListID = $listID";
+function getAllTasks($listID, $status) {
+    if ($status == 'open') {
+        $statusID = 0;
+    } else if ($status == 'done') {
+        $statusID = 1;
+    }
+    $sql = "SELECT taskID, taskName, taskStatus FROM TodoApp.Tasks WHERE taskListID = $listID AND taskStatus = $statusID";
     return SQLQuery($sql);
 }
 
